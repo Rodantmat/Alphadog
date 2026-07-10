b41850c35db1e23e972f17fc3f2b95ac675bb15d
Rodantmat <129540342+Rodantmat@users.noreply.github.com>
2026-07-10 16:42:25 -0700

    Clean up ctx object #1 - remove unused latestBaselinePass field

--- alphadog-v2-delta-certifier.js
+++ alphadog-v2-delta-certifier.js
@@ -1226,7 +1226,7 @@ async function rebuildCoverage(env, batchId, requestId, startDate, endDate, opti
         }
       }
       if (includeBaselineV5Coverage) {
-        for (const layerKey of BASELINE_V5_COVERAGE_LAYER_KEYS) addLayer(g, baselineV5CoverageLayerFromState(layerKey, g, { latestBaselinePass, existingBaselineCoverage, baselineV5DailyStateValidity, calendarExceptionNoStatsExpected: true, currentOrFutureNonFinal, liveSourceRowsForGame }));
+        for (const layerKey of BASELINE_V5_COVERAGE_LAYER_KEYS) addLayer(g, baselineV5CoverageLayerFromState(layerKey, g, { existingBaselineCoverage, baselineV5DailyStateValidity, calendarExceptionNoStatsExpected: true, currentOrFutureNonFinal, liveSourceRowsForGame }));
       }
     } else if (!evaluateLiveLayers) {
       for (const layerKey of activeLayers) {
