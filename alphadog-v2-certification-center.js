69a62f7b44579e7aead49bd549a9394f1be79844
Rodantmat <129540342+Rodantmat@users.noreply.github.com>
2026-07-14 00:08:45 -0700

    Rewire non-quota block FROM clause

--- alphadog-v2-certification-center.js
+++ alphadog-v2-certification-center.js
@@ -845,7 +845,7 @@ function buildCurrentSql(url) {
     LIMIT ${limit}
   ` : `
     SELECT ${baseSelect}
-    FROM v2_final_board_current f
+    FROM score_final_board_current f
     LEFT JOIN score_board_prepared_current p ON p.prepared_row_id=f.prepared_row_id
     ${where.length ? "WHERE " + where.join(" AND ") : ""}
     ORDER BY COALESCE(f.rank_order,999999) ASC
