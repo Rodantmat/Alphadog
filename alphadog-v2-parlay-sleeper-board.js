bf939e491b60353d3f2f25cd4fdf90361e0e416d
Rodantmat <129540342+Rodantmat@users.noreply.github.com>
2026-07-24 11:19:49 -0700

    Same connect_timeout fix.

--- alphadog-v2-parlay-sleeper-board.js
+++ alphadog-v2-parlay-sleeper-board.js
@@ -114,7 +114,7 @@ function allTrue(obj) { return Object.values(obj).every(Boolean); }
 async function readJsonSafe(request) { try { return await request.json(); } catch (_) { return {}; } }
 
 function pgClient(env) {
-  return postgres(env.HYPERDRIVE.connectionString, { max: 3, fetch_types: false, prepare: false });
+  return postgres(env.HYPERDRIVE.connectionString, { max: 3, fetch_types: false, prepare: false, connect_timeout: 8 });
 }
 
 async function ensureSleeperSchema(env) {
