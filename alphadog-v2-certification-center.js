db017629669a90a1b305d41012ac6c359b34305f
Rodantmat <129540342+Rodantmat@users.noreply.github.com>
2026-07-25 19:06:11 -0700

    Bump version again per explicit instruction to always do this for cache freshness.

--- alphadog-v2-certification-center.js
+++ alphadog-v2-certification-center.js
@@ -1,7 +1,7 @@
 import postgres from "postgres";
 const WORKER_NAME = "alphadog-v2-certification-center";
 const LOGICAL_APP = "alphadog-v2-main-ui";
-const VERSION = "alphadog-v2-certification-center-v0.2.2-position-test";
+const VERSION = "alphadog-v2-certification-center-v0.2.3-global-error-handler";
 const JOB_KEY = "main-ui-board-viewer";
 
 const REQUIRED_DB_BINDINGS = ["CONTROL_DB", "CONFIG_DB", "REF_DB", "STATS_HITTER_DB", "STATS_PITCHER_DB", "TEAM_DB", "DAILY_DB", "MARKET_DB", "CONTEXT_DB", "SCORE_DB", "ARCHIVE_DB"];
