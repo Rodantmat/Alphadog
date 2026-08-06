7720f2d5e43c81fcc957b017305059dab67f163d
Rodantmat <129540342+Rodantmat@users.noreply.github.com>
2026-08-06 15:15:31 -0700

    Retry attempt.

--- generate_wrangler_configs.py
+++ generate_wrangler_configs.py
@@ -20,6 +20,7 @@ def master_run_crons(minute_offset):
     return [f"{minute_offset} {h} * * *" for h in MASTER_RUN_BASE_TIMES]
 
 
+
 def service_binding_name(worker_name):
     return worker_name.replace("alphadog-v2-", "").replace("-", "_").upper() + "_WORKER"
 
